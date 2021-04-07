612d525dab0538edff5f95eda395ee6012c0218a
Jason Kim <kimks3@mcmaster.ca>
2021-04-07 17:05:25 -0400

    Improved error message clarity, fixed empty date object error message, cleaned up code

--- code/parser.js
+++ code/parser.js
@@ -1,8 +1,7 @@
 // Main program code for COMP SCI 4TB3 Group 9 Project - Natural Language to iCalendar converter
 // Open index.html in a web browser for demo webpage. The resources directory contains supporting files/framework/libraries
 //
-// This program makes use of the following open source libraries and frameworks: 
-// TODO REFERENCES HERE
+// This program makes use of the following open source libraries and frameworks: ics.js, pure.io
 //
 // ==========================================================
 
@@ -42,8 +41,10 @@ function splitAtPeriod(input) {
 	// Split description from rest of input	
 	splitted = input.split('. ');
 	
-	if (splitted[1]){ eventDescription = splitted[1]; } 
-	else { eventDescription = "No description"; }
+	if (splitted[1])
+	eventDescription = splitted[1];
+	else
+	eventDescription = "No description"; 
 	
 	splitSummaryDate(splitted[0])
 	
@@ -54,22 +55,20 @@ function splitAtPeriod(input) {
 function splitSummaryDate(input){
 	
 	// Split summary from rest of input
-	if (input.search(' on ') > 0){
-		var splitted = input.split(' on ')
-	} 
-	else if (input.search(' at ') > 0){
-		var splitted = input.split(' at ')
-	} 
-	else if (input.search(' from ') > 0){
-		var splitted = input.split(' from ')
-	}
-	else if (input.search(' between') > 0){
-		var splitted = input.split(' between ')
-	}
+	if (input.search(' on ') > 0)
+	var splitted = input.split(' on ')
+	else if (input.search(' at ') > 0)
+	var splitted = input.split(' at ')
+	else if (input.search(' by ') > 0)
+	var splitted = input.split(' by ')
+	else if (input.search(' from ') > 0)
+	var splitted = input.split(' from ')
+	else if (input.search(' between') > 0)
+	var splitted = input.split(' between ')
 	else { // If no summary-date separator in input, error
 		eventSummary = input
 		eventBegin = error("Could not find summary-date separator (Error S1)")
-		eventEnd = ""
+		eventEnd = "No date or time"
 		return
 	}
 	
@@ -97,9 +96,10 @@ function parseDateTime(input) {
 		} else {
 		eventBegin = parseAbsoluteDateTime(input);
 		eventEnd = parseAbsoluteDateTime(input);
-		if (typeof eventEnd === Date) {
-			eventEnd.setHours(eventEnd.getHours() + 1); //Default event length is 1hr
-		} 
+		
+		if (typeof eventEnd === Date)
+		eventEnd.setHours(eventEnd.getHours() + 1); //Default event length is 1hr
+		
 		return;
 	}
 }
@@ -125,11 +125,9 @@ function parseRelativeDateTime(input){
 		date.setDate(date.getDate() + 7);
 	}
 	
-	eventBegin = date;
-	
+	eventBegin = date;	
 	eventEnd = date;
-	eventEnd.setHours(eventEnd.getHours() + 1); //Default event length is 1hr
-	
+	eventEnd.setHours(eventEnd.getHours() + 1); //Default event length is 1hr	
 	return;
 }
 
@@ -148,27 +146,26 @@ function parseDateTimeRange(input){
 }
 
 function parseAbsoluteDateTime(input){
+	// If date is missing altogether, reject
+	if (!input || input == " ")
+	return error("Could not find a date value (Error D1)")
+	
 	const referenceDate = new Date() // Reference Date
 	var date = new Date() // Date Time to modify
 	date.setHours(9, 0, 0) // Default time to 9am
 	
-	// Try to initially create a Date Time object using constructor, return if successfull
+	// Try to initially create a Date Time object using constructor, return if successful
 	var dateAttempt = new Date(input)
-	if (dateAttempt != 'Invalid Date') { return dateAttempt; }
+	if (dateAttempt != 'Invalid Date')
+	return dateAttempt;
 	
-	dayMatchArray = input.toLowerCase().match(regExDayofWeek) //Convert date to lowercase then match w/ regex
+	// If no good, try to parse as a relative date
+	dayMatchArray = input.toLowerCase().match(regExDayofWeek) 
 	
-	if (dayMatchArray){ 
-		date = setDateByDayOfWeek(date, dayMatchArray, referenceDate)
-		} else { 
-		return error("Could not find acceptable date/time in input (Error D1)") // If blank date arg received, Error
-	}
-	
-	if (date == 'Invalid Date') {
-		return error("Could not parse <i>" + input + "</i> as a date (Error D2)") // If date object is invalid, Error
-	}
-	
-	return date;
+	if (dayMatchArray)
+	return setDateByDayOfWeek(date, dayMatchArray, referenceDate)
+	else
+	return error("Could not parse <i>" + input + "</i> as a date (Error D2)") // Date is unrecognizable
 }
 
 
@@ -203,17 +200,12 @@ function setDateByDayOfWeek(date, dayMatchArray, referenceDate){
 	return date;
 }
 
+
 // Generate properly formatted .ICS file (once user hits enter or clicks download btn. Arg 1 = download, arg 0 = view only
-function generateICS(arg) {	
-	// If no description, use empty string for file rather than "No description"
-	if (eventDescription == "No description")
-	var icsDescription = "";
-	else 
-	var icsDescription = eventDescription;
-	
-	// Build the iCalendar file using ics.js library and parsed data
+function generateICS(arg) {		
+	// Build the iCalendar file using ics.js library and parsed data. If no description, use empty string for file rather than "No description"
 	icalOutput = ics();
-    icalOutput.addEvent(eventSummary, icsDescription, '', eventBegin, eventEnd);
+	icalOutput.addEvent(eventSummary, eventDescription == "No description" ? '' : eventDescription, '', eventBegin, eventEnd);
 	
 	// Give user the .ics or display the preview
 	if (arg) {
@@ -228,17 +220,17 @@ function generateICS(arg) {
 
 // Format live output HTML
 function formatHTML(eventSummary, eventBegin, eventEnd, eventDescription) {	
-	// Format date and time 
-	if (inputGood) {
-		const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-		eventBegin = eventBegin.toLocaleDateString("en-US", options) + ' ' + eventBegin.toLocaleTimeString();
-		eventEnd = eventEnd.toLocaleDateString("en-US", options) + ' ' + eventEnd.toLocaleTimeString();
-	}
+	// Format date and time in en-US locale (try catch required since variable may contain non-date object)
+	const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	try {eventBegin = eventBegin.toLocaleDateString("en-US", options) + ' ' + eventBegin.toLocaleTimeString();}
+	catch {}
+	try {eventEnd = eventEnd.toLocaleDateString("en-US", options) + ' ' + eventEnd.toLocaleTimeString();}
+	catch {}
 	
 	// Disable download button if the current input is unacceptable and update help text
 	if (!inputGood) {
 		document.getElementById("btnDownload").disabled = true;
-		document.getElementById("helpText").innerHTML = 'Please fix the issues above to download your iCalendar file.';
+		document.getElementById("helpText").innerHTML = '⚠️ Please fix the issues above to download your iCalendar file.';
 	}
 	else {
 		document.getElementById("btnDownload").disabled = false; 
@@ -254,7 +246,7 @@ function formatHTML(eventSummary, eventBegin, eventEnd, eventDescription) {
 }
 
 
-// Format error strings in red. Called by splitInput() and parseDate() when an error occurs
+// Format error strings in red and mark input as no good
 function error(errorString) {
 	inputGood = 0 // Mark user input as unacceptable once error occurs
 	
