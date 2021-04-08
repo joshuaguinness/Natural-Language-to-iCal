2c1cfd871fcac6a8b5f4dd3e6f48dc59001c9599
Arkin Modi <modia1@mcmaster.ca>
2021-04-08 18:57:24 -0400

    Created a test for setDateByDayOfWeek

--- code/parser.test.js
+++ code/parser.test.js
@@ -1,1 +1,12 @@
-const parser = require('./parser');
+const parser = require("./parser");
+
+// TODO: Create tests for regex
+
+// setDateByDayOfWeek tests
+test("setDateByDayOfWeek: set to next Monday", () => {
+    const referenceDate = new Date("April 12, 2021 09:00:00"); // Monday
+    const dayMatchArray = ["mon", "monday"];
+    let date = new Date("April 12, 2021 09:00:00");
+    let event = parser.setDateByDayOfWeek(date, dayMatchArray, referenceDate);
+    expect(event.getDate()).toBe(19);
+});
